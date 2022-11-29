fcaf943e651826b20dd8744bd5e008bcd0184bc1
Juan Cruz <juancruzperez@gmail.com>
2022-11-28 21:56:36 -0300

    landing bugeada
    landing bugeada

--- client/src/component/Landing/landing.js
+++ client/src/component/Landing/landing.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function Landing(){
-    return(
-     <div>
-        <h1>holaaaaa</h1>
-    </div>   
-    )
-    
+export default function Landing() {
+  return (
+    <div>
+      <h1>holaaaaa</h1>
+      <h2>hola pianola carita</h2>
+    </div>
+  );
 }
