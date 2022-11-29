b7d489311c970518651fca27d916dff751669220
TinoProgramer <103002616+TinoProgramer@users.noreply.github.com>
2022-11-28 21:55:38 -0300

    landing como esta
    mod landing

--- client/src/component/Landing/landing.js
+++ client/src/component/Landing/landing.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function Landing(){
-    return(
-     <div>
-        <h1>holaaaaa</h1>
-    </div>   
-    )
-    
+export default function Landing() {
+  return (
+    <div>
+      <h1>holaaaaa</h1>
+      <h2>como esta usted?</h2>
+    </div>
+  );
 }
